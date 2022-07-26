38e6016d5925366beecf8ddb16c67a034781e0ae
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-07-26 15:50:41 +0300

    changed to hooks

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import Statistics from 'components/Statistics';
+import Notification from 'components/Notification';
+import Section from 'components/Section';
+import FeedbackOptions from 'components/FeedbackOptions';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        setBad(prevState => prevState + 1);
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.floor((good / countTotalFeedback()) * 100);
+  };
+
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleIncrement}
+        ></FeedbackOptions>
+      </Section>
+
+      <Section title="Statistics">
+        {!countTotalFeedback() ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            percent={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </div>
+  );
+}
---
+++ src/components/Notification.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import styles from 'styles/styles.module.css';
+
+const Notification = ({ message }) => <p className={styles.text}>{message}</p>;
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+export default Notification;
